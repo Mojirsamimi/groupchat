37b302cf2580666b9abae0ea47fc6a4e390b1f9e
Mojir Samimi <mojir.s@gmail.com>
2016-05-10 00:57:54 -0400

    first commit

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -5,7 +5,7 @@ class ChatController < ApplicationController
     chat.chatroom_id=params[:id]
     chat.ch_text=params[:chat][:ch_text]
      @chat1=Chatroom.find(params[:id]).chats
-     Pusher['test_channel'].trigger('chatroom'+params[:id].to_s, {:chat_html => render(partial: 'chatroom/content')})
+     Pusher['test_channel'].trigger('chatroom'+params[:id].to_s, {:chat_html => "helolo"})
 
     respond_to do |format|
       if chat.save
